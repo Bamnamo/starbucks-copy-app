cb295f74323ea3dc2e8bfe24d9608ac5e9c19256
bamnamo <shivash@naver.com>
2025-03-27 09:54:39 +0900

    scrollMagic -> gsap scrollTrigger

--- js/main.js
+++ js/main.js
@@ -145,20 +145,35 @@ floatingObject(".floating1", 1, 15);
 floatingObject(".floating2", 0.5, 15);
 floatingObject(".floating3", 1.5, 20);
 
-/**
- * 요소가 화면에 보여짐 여부에 따른 요소 관리
- */
-// 관리할 요소들 검색!
+// /**
+//  * 요소가 화면에 보여짐 여부에 따른 요소 관리
+//  */
+// // 관리할 요소들 검색!
+// const spyEls = document.querySelectorAll("section.scroll-spy");
+// // 요소들 반복 처리!
+// spyEls.forEach(function (spyEl) {
+//   new ScrollMagic.Scene({
+//     // 감시할 장면(Scene)을 추가
+//     triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+//     triggerHook: 0.8, // 화면의 80% 지점에서 보여짐 여부 감시
+//   })
+//     .setClassToggle(spyEl, "show") // 요소가 화면에 보이면 show 클래스 추가
+//     .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+// });
+
+// 요소 선택
 const spyEls = document.querySelectorAll("section.scroll-spy");
-// 요소들 반복 처리!
-spyEls.forEach(function (spyEl) {
-  new ScrollMagic.Scene({
-    // 감시할 장면(Scene)을 추가
-    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
-    triggerHook: 0.8, // 화면의 80% 지점에서 보여짐 여부 감시
-  })
-    .setClassToggle(spyEl, "show") // 요소가 화면에 보이면 show 클래스 추가
-    .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수!)
+
+// 요소 반복 처리
+spyEls.forEach((spyEl) => {
+  gsap.to(spyEl, {
+    scrollTrigger: {
+      trigger: spyEl, // 트리거 요소
+      start: "top 80%", // 요소의 top이 뷰포트의 80% 지점에 도달할 때 시작
+      toggleClass: "show", // 클래스 토글
+      once: true, // 한 번만 작동 (원하면 false로 변경)
+    },
+  });
 });
 
 const thisYearv = document.querySelector(".this-year");
